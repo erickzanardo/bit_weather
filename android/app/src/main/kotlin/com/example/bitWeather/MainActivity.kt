297b65622899f0b1b18e8109e14cca0984ca0783
Erick Zanardo <erickzanardoo@gmail.com>
2021-05-08 14:13:36 -0300

    Reseting platform folders

--- android/app/src/main/kotlin/com/example/bitWeather/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bitWeather
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/bit_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bit_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
