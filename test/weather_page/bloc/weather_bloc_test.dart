356fe0879fad07e1e557d514391b5480155b16aa
Erick Zanardo <erickzanardoo@gmail.com>
2021-05-09 10:37:25 -0300

    Adding WeatherBlock test

---
+++ test/weather_page/bloc/weather_bloc_test.dart
@@ -0,0 +1,83 @@
+import 'package:bit_weather/models/location.dart';
+import 'package:bit_weather/models/weather.dart';
+import 'package:bit_weather/models/weather_location.dart';
+import 'package:bit_weather/weather_page/bloc/weather_bloc.dart';
+import 'package:bit_weather/weather_page/bloc/weather_event.dart';
+import 'package:bit_weather/weather_page/bloc/weather_state.dart';
+import 'package:test/test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+import 'package:bit_weather/repositories/weather_repository.dart';
+
+class WeatherRepositoryMock extends Mock implements WeatherRepository {}
+
+void main() {
+  final weatherLocation = WeatherLocation(
+    location: Location(
+      title: 'Treviso',
+      woeid: 20,
+    ),
+    weather: Weather(
+      id: 1,
+      minTemp: 10,
+      maxTemp: 20,
+      currentTemp: 15,
+      windSpeed: 10,
+      weatherState: WeatherType.clear,
+    ),
+  );
+  group('WeatherPage - WeatherBloc', () {
+    blocTest<WeatherBloc, WeatherState>(
+      'Searchs a city and emits WeatherLoaded',
+      build: () {
+        final repository = WeatherRepositoryMock();
+        when(() => repository.fetchWeather('Treviso')).thenAnswer(
+          (_) async {
+            return weatherLocation;
+          },
+        );
+
+        return WeatherBloc(repository: repository);
+      },
+      act: (bloc) => bloc.add(const WeatherSearch('Treviso')),
+      expect: () => [
+        WeatherLoading(),
+        WeatherLoaded(weather: weatherLocation),
+      ],
+    );
+
+    blocTest<WeatherBloc, WeatherState>(
+      'Emits WeatherNotFound when the city can\'t be found',
+      build: () {
+        final repository = WeatherRepositoryMock();
+        when(() => repository.fetchWeather('Pallet')).thenAnswer(
+          (_) async => null,
+        );
+
+        return WeatherBloc(repository: repository);
+      },
+      act: (bloc) => bloc.add(const WeatherSearch('Pallet')),
+      expect: () => [
+        WeatherLoading(),
+        WeatherNotFound(),
+      ],
+    );
+
+    blocTest<WeatherBloc, WeatherState>(
+      'Emits WeatherLoadingFailed when the repository throws and error',
+      build: () {
+        final repository = WeatherRepositoryMock();
+        when(() => repository.fetchWeather('Pallet'))
+            .thenThrow('Some problems');
+
+        return WeatherBloc(repository: repository);
+      },
+      act: (bloc) => bloc.add(const WeatherSearch('Pallet')),
+      expect: () => [
+        WeatherLoading(),
+        WeatherLoadingFailed(),
+      ],
+    );
+  });
+}
