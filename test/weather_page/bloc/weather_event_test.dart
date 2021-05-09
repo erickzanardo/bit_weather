4b0e21cd74174044f7cfce2f6e8fe9c1c7b0b556
Erick Zanardo <erickzanardoo@gmail.com>
2021-05-09 14:07:48 -0300

    Adding weather search test

---
+++ test/weather_page/bloc/weather_event_test.dart
@@ -0,0 +1,20 @@
+import 'package:bit_weather/weather_page/bloc/weather_event.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('WeatherPage - WeatherSearch', () {
+    test('objects with equal city name are equal', () {
+      final event =  const WeatherSearch('Rome');
+      final anotherEvent =  const WeatherSearch('Rome');
+
+      expect(event, anotherEvent);
+    });
+
+    test('objects with different city name are not equal', () {
+      final event =  const WeatherSearch('Treviso');
+      final anotherEvent =  const WeatherSearch('Rome');
+
+      expect(event, isNot(equals(anotherEvent)));
+    });
+  });
+}
